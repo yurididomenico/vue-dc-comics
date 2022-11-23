05ba1dd06f00be23aaa094ad6a357bd8f0a491c6
yurididomenico <yurididomenico93@gmail.com>
2022-11-23 17:53:53 +0100

    Main: Compilazione Shop

--- src/components/Main/MainShopComponent.vue
+++ src/components/Main/MainShopComponent.vue
@@ -2,11 +2,11 @@
     <div class="bg-azzurro">
         <div class="contenitore65 padding-y-40">
             <ul class="flex-mode f-justify-around f-align-center">
-                <li><span>[]</span><span>Testo</span></li>
-                <li><span>[]</span><span>Testo</span></li>
-                <li><span>[]</span><span>Testo</span></li>
-                <li><span>[]</span><span>Testo</span></li>
-                <li><span>[]</span><span>Testo</span></li>
+                <li><a href="#" class="flex-mode f-align-center"><img src="../../assets/img/buy-comics-digital-comics.png" alt="Icona Digital Comics"><span>DIGITAL COMICS</span></a></li>
+                <li><a href="#" class="flex-mode f-align-center"><img src="../../assets/img/buy-comics-merchandise.png" alt="Icona Digital Comics"><span>DC MERCHANDISE</span></a></li>
+                <li><a href="#" class="flex-mode f-align-center"><img src="../../assets/img/buy-comics-subscriptions.png" alt="Icona Digital Comics"><span>SUBSCRIPTION</span></a></li>
+                <li><a href="#" class="flex-mode f-align-center"><img src="../../assets/img/buy-comics-shop-locator.png" alt="Icona Digital Comics" class="width-"><span>COMIC SHOP LOCATOR</span></a></li>
+                <li><a href="#" class="flex-mode f-align-center"><img src="../../assets/img/buy-dc-power-visa.svg" alt="Icona Digital Comics"><span>SC POWER VISA</span></a></li>
             </ul>
         </div>
     </div>
@@ -27,9 +27,25 @@
         background-color: #0282f9;
     }
 
-    ul li span
+    ul
     {
-        padding: 0 5px;
+        li
+        {
+            span
+            {
+                padding: 0 15px;
+            }
+
+            img
+            {
+                width: 40px;
+            }
+
+            .width-
+            {
+                width: 30px;
+            }
+            
+        }
     }
-    
 </style>
