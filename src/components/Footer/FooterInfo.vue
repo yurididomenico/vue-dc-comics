f6029b3ea824e4a54c8b038895d0251a058685e4
yurididomenico <yurididomenico93@gmail.com>
2022-11-23 18:30:54 +0100

    Footer: Compilazione Info

--- src/components/Footer/FooterInfo.vue
+++ src/components/Footer/FooterInfo.vue
@@ -3,47 +3,47 @@
         <div class="contenitore65 padding-y-40 flex-mode">
             <div>
                 <ul>
-                    <li><h2>Titolo</h2></li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
+                    <li><h2>DC COMICS</h2></li>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
                 </ul>
                 <ul>
-                    <li><h2>Titolo</h2></li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                </ul>
-            </div>
-
-            <div>
-                <ul>
-                    <li><h2>Titolo</h2></li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
+                    <li><h2>SHOP</h2></li>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
                 </ul>
             </div>
 
             <div>
                 <ul>
-                    <li><h2>Titolo</h2></li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
-                    <li>Testo</li>
+                    <li><h2>DC</h2></li>
+                    <li>Terms Of Use</li>
+                    <li>Privacy policy (New)</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talen Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Rating</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    <li><h2>SITES</h2></li>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
                 </ul>
             </div>            
         </div>
